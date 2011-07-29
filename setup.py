9a7d0cd0ff71a02fd31311b726f50f70a39a96eb
Adeel Ahmad Khan <adeel@adeel.ru>
2011-07-28 23:24:26 -0400

    bump to version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name="pump-flash",
-  version="0.0.1",
+  version="0.0.2",
   description="A Pump middleware for adding flash message support via sessions.",
   author="Adeel Ahmad Khan",
   author_email="adeel@adeel.ru",
